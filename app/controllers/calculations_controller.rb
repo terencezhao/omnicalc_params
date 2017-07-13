ad55b5113fe2e5dec345e5f63d36f1c3ecf457b6
Terence Zhao <terencezhao@chicagobooth.edu>
2017-07-13 01:58:50 +0000

    finished all except Flexible Payment Example

---
+++ app/controllers/calculations_controller.rb
@@ -0,0 +1,31 @@
+class CalculationsController < ApplicationController
+   def flex_square
+      @user_number = params["number"].to_i
+      @squared_number = @user_number ** 2
+      render("calculations/flexible_square_template.html.erb")
+   end
+   
+   def flex_square_root
+      @user_number = params["number"].to_i
+      @square_root_number = Math.sqrt(@user_number)
+      render("calculations/flexible_square_root_template.html.erb")
+   end
+   
+   def flex_payment
+      @apr = (params["apr"].to_i / 100.0)
+      @years = params["years"].to_i
+      @principal = params["principal"].to_i
+      @payment = ((@apr/12.0 * @principal) / (1.0 - (1.0 + @apr/12.0)**(-@years*12)))
+      render("calculations/flexible_payment_template.html.erb")
+   end
+   
+   def square_form
+      render("calculations/square_form_template.html.erb")
+   end
+   
+   def square_form_results
+      @number = params["number"].to_f
+      @square = @number ** 2
+      render("calculations/square_form_results_template.html.erb")
+   end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  get("/flexible/square/:number", { :controller => "calculations", :action => "flex_square" })
+  get("/flexible/square_root/:number", { :controller => "calculations", :action => "flex_square_root" })
+  get("/flexible/payment/:apr/:years/:principal", { :controller => "calculations", :action => "flex_payment" })
+  get("/flexible/random/:start/:end", { :controller => "calculations", :action => "flex_random" })
+
+  get("/square/new", { :controller => "calculations", :action => "square_form" })
+  get("/square/results", { :controller => "calculations", :action => "square_form_results"})
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   mount WebGit::Engine, at: "/rails/git"
 end
